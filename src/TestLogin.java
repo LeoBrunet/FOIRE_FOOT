1153d9737c66f761aea86611f2e8d8fd43803dc9
LeoBrunet <leobrunet91@gmail.com>
2021-12-03 16:40:21 +0100

    add jars

--- src/TestLogin.java
+++ src/TestLogin.java
@@ -6,7 +6,6 @@ import exceptions.WrongPasswordException;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.junit.Test;
 
-import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.*;
