543a94f2b4e0264d710f659970ed3754538468bd
Cantero <clement.cantero@etu.umontpellier.fr>
2022-01-04 15:52:44 +0100

    affichage teams non finis v2

--- main/java/com/foirfoot/view/team/TeamController.java
+++ main/java/com/foirfoot/view/team/TeamController.java
@@ -1,34 +1,53 @@
 package com.foirfoot.view.team;
 
+import com.foirfoot.model.Facade;
 import com.foirfoot.model.club.Club;
 import com.foirfoot.model.team.Team;
 import com.foirfoot.model.user.User;
 import com.foirfoot.view.Controller;
 import com.foirfoot.view.Main;
 import javafx.fxml.FXML;
-import javafx.scene.image.Image;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 
-public class TeamController extends Controller {
-    private Club club;
+import java.util.Optional;
 
+public class TeamController extends Controller {
+    private final Facade facade = new Facade();
+    private Team team;
     @FXML
     private Text teamName;
     @FXML
-    private VBox teams;
-
+    private Text numberPlayer;
     @FXML
-    public void initialize(){
+    private Text category;
+    @FXML
+    private Text type;
+    @FXML
+    private VBox players;
+    @FXML
+    private VBox coaches;
+
+    public void initialize() {
         System.out.println("initialize()");
-        for (Team t : club.getTeams()) {
-            System.out.println(t);
-            teams.getChildren().add(new Text(t.getType() + " " + t.getCategory()));
+        teamName.setText(team.getType() + " - " + team.getCategory() + " - " + team.getClub().getName());
+        /*numberPlayer.setText(String.valueOf(team.getPlayers().size()));*/
+        category.setText(team.getCategory());
+        type.setText(team.getType());
+        Optional<Team> t = facade.getTeam(team.getId());
+        System.out.println("uifgeeeeee");
+        System.out.println(t);
+
+        /*for (User p : team.getPlayers()) {
+            players.getChildren().add(new Text(p.getFirstName() + " " + p.getName()));
         }
+        for (User c : team.getCoachs()) {
+            coaches.getChildren().add(new Text(c.getFirstName() + " " + c.getName()));
+        }*/
     }
 
     @Override
     public void setParameter(Object[] params) {
-        this.club = (Club) params[0];
+        this.team = (Team) params[0];
     }
 }
