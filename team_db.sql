43fa08a2775a18d2db8dc1af92a2b8a3ac0867a6
Cantero <clement.cantero@etu.umontpellier.fr>
2022-01-11 10:18:58 +0100

    teams_db

---
+++ team_db.sql
@@ -0,0 +1,48 @@
+DROP TABLE IF EXISTS PRODUCT RESTRICT;
+DROP TABLE IF EXISTS TRANSACTION RESTRICT;
+DROP TABLE IF EXISTS ADDRESS RESTRICT;
+DROP TABLE IF EXISTS BASKET RESTRICT;
+CREATE TABLE TEAMS
+(
+    team_id         MEDIUMINT    NOT NULL AUTO_INCREMENT,
+    team_name      varchar(255) NOT NULL UNIQUE,
+    club_id  MEDIUMINT NOT NULL,
+
+    category varchar(255) NOT NULL,
+    type       varchar(255) NOT NULL,
+    PRIMARY KEY (team_id),
+    FOREIGN KEY (club_id) REFERENCES CLUBS (club_id)
+)ENGINE = InnoDB
+ DEFAULT CHARSET = utf8;
+
+CREATE TABLE RESULTS
+(
+    result_id         MEDIUMINT    NOT NULL AUTO_INCREMENT,
+    score_ht        MEDIUMINT,
+    score_ot      MEDIUMINT,
+
+    home_team MEDIUMINT NOT NULL,
+    outside_team      MEDIUMINT NOT NULL,
+    FOREIGN KEY (home_team) REFERENCES TEAMS (team_id),
+    FOREIGN KEY (outside_team) REFERENCES TEAMS (team_id)
+    PRIMARY KEY (result_id)
+)ENGINE = InnoDB
+ DEFAULT CHARSET = utf8;
+
+CREATE TABLE CATEGORIES
+(
+
+    cat_id         MEDIUMINT    NOT NULL AUTO_INCREMENT,
+    cat_name varchar(255) NOT NULL,
+    PRIMARY KEY (cat_id)
+)ENGINE = InnoDB
+ DEFAULT CHARSET = utf8;
+
+CREATE TABLE TYPES
+(
+
+    type_id         MEDIUMINT    NOT NULL AUTO_INCREMENT,
+    type_name varchar(255) NOT NULL,
+    PRIMARY KEY (type_id)
+)ENGINE = InnoDB
+ DEFAULT CHARSET = utf8;
